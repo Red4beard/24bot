48469fd94767d51de16078b1f317e4cef50347ca
Aleksandr Chikurov <alchikurov@gmail.com>
2025-06-07 17:37:42 +0200

    редизайн лендинга 24bot.org

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro 24bot Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Автоматизация и умные боты для роста бизнеса";
+export const SITE_DESCRIPTION = "Автоматизация, которая работает за вас 24/7";
